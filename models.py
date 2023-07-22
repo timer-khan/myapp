815a819e8f60576f70cf506da9570215a2e68ade
timer-khan <139461914+timer-khan@users.noreply.github.com>
2023-07-22 13:46:47 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,17 @@
+# myapp/models.py
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
