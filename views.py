a40e3db288e60b4115822691890653c9a6755119
timer-khan <139461914+timer-khan@users.noreply.github.com>
2023-07-22 13:47:00 +0200

    Create views.py

---
+++ views.py
@@ -0,0 +1,7 @@
+# myapp/views.py
+from django.shortcuts import render
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'myapp/product_list.html', {'products': products})
